7933d33d79fe3e7953f827045213a83e3cf17170
yanis khellaf <yanis.khellaf@outlook.fr>
2022-06-15 15:35:23 +0200

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "restaurants#index"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
 end
